f2b3ba414cb72e20f81455f19f273169d6d54eb5
Roman <31464179+grand-roman@users.noreply.github.com>
2022-09-25 22:18:08 +0700

    create task

--- counter-application/counter-application/ViewController.swift
+++ counter-application/counter-application/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numb: Int = 0
+    @IBOutlet weak var numberСounter: UILabel!
+    @IBOutlet weak var incrementButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        numberСounter.text = "Значение счётчика:\n \(numb)"
+        
     }
 
-
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        if sender.backgroundColor == .link {
+            sender.backgroundColor = .systemCyan
+        }
+        else if sender.backgroundColor == .systemCyan {
+            sender.backgroundColor = .link
+        }
+        self.numb += 1
+        if self.numb >= 100 {
+            numberСounter.textColor = .red
+        }
+        numberСounter.text = "Значение счётчика:\n \(numb)"
+        
+    }
+    
 }
-
